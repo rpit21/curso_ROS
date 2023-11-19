bff608d373ac6a7b2b570cbc8948e1b37be0c288
rpit21 <rubennevaldo@gmail.com>
2023-11-19 14:08:24 -0500

    funcionamiento de launch para operacion por teclado y por joy control

---
+++ motion_plan/src/joy_sub_pub.cpp
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <iostream>
+ros::Publisher vel_pub;
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
+  geometry_msgs::Twist twist;
+  twist.angular.z = 0.2*joy->axes[3];
+  twist.linear.x = 0.4*joy->axes[1];
+  vel_pub.publish(twist);
+  //std::cout<<"velocidad "<<twist.angular.z <<"  "<<twist.linear.x<<std::endl;
+	
+	}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "joy_sub_pub");
+  ros::NodeHandle  nh;
+  ros::Subscriber sub = nh.subscribe("joy",10,joyCallback);
+  vel_pub =nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+  ros::spin();
+}
